02a04c689cb2dd1c3455da825fefb1138b0528c6
Backer <AsanRodnuan2546@gmail.com>
2024-09-04 12:08:58 +0700

    feat: Add registration form for new users

--- save_regis.php
+++ save_regis.php
@@ -1,16 +1,49 @@
-<?php
+<?php    
+    $users_prefix = $_POST['user_prefix'];
+    $users_name = $_POST['user_name'];
+    $users_lastname = $_POST['user_lastname'];
+    $users_mail = $_POST['user_mail'];
+    $users_password = $_POST['user_password'];
+    $users_date = $_POST['user_birthday'];
+    $users_sex = $_POST['user_sex'];
+    $users_code = $_POST['user_ids'];
+    $users_phone = $_POST['user_tel'];
+    $users_add = $_POST['user_address'];
 
-    echo $_POST['user_prefix'];
-    echo $_POST['user_name'];
-    echo $_POST['user_lastname'];
-    echo $_POST['user_mail'];
-    echo $_POST['user_birthday'];
-    echo $_POST['user_sex'];
-    echo $_POST['user_ids'];
-    echo $_POST['user_tel'];
-    echo $_POST['user_password'];
-    echo $_POST['user_address'];
+    $hash_pasword = password_hash($users_password, PASSWORD_BCRYPT);
+    $localhost = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "cdte_65_1";
+    $conn = new mysqli($localhost, $username, $password, $database);
+    $conn->set_charset("utf8");
+    if($conn->connect_error){
+        die("Connection failed: ".$conn->connect_error);
+    }
+
+    $sql = "INSERT INTO users (users_prefix,
+    users_name, users_lastname, users_mail,
+    users_password, users_date, users_sex,
+    users_code, users_phone, users_add, users_regis) VALUES ('$users_prefix',
+    '$users_name', '$users_lastname', '$users_mail',
+    '$hash_pasword', '$users_date', '$users_sex',
+    '$users_code', '$users_phone', '$users_add', NOW())";
+
+    echo $hash_pasword;
+
+
+    if($conn->query($sql) === TRUE){
+        echo "<script>
+        alert('บันทึกข้อมูลเรียบร้อยแล้ว');
+      </script>";
+
     
-    
+    }else{
+        echo "ควายบันทึกไม่ได้หรอก แบร่ๆๆๆ";
+    }
+
+
+
+
 
 ?>
